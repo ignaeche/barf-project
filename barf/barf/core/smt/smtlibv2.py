fe62f90df897ccd4db1d56ea4c7a5134dd3d5113
g <g@g>
2015-03-04 10:46:08 -0300

    fixed local z3 path

--- barf/barf/core/smt/smtlibv2.py
+++ barf/barf/core/smt/smtlibv2.py
@@ -526,6 +526,7 @@ class Z3Solver(object):
         self._declarations = {} #weakref.WeakValueDictionary()
         self._constraints = set()
         self.input_symbols = list()
+        #assert(0)
         self._z3_path = '~/.local/bin/z3'
         self._proc = Popen(self._z3_path + ' -t:120 -smt2 -in', shell=True, stdin=PIPE, stdout=PIPE)
 
@@ -550,7 +551,7 @@ class Z3Solver(object):
         self._constraints = state['constraints']
         self._stack = state['stack']
         self.input_symbols = state['input_symbols']
-        self._proc = Popen('z3 -t:120 -smt2 -in', shell=True, stdin=PIPE, stdout=PIPE)
+        self._proc = Popen(self._z3_path + ' -t:120 -smt2 -in', shell=True, stdin=PIPE, stdout=PIPE)
 
     def reset(self, full=False):
         self._send("(reset)")
@@ -567,7 +568,7 @@ class Z3Solver(object):
             self._declarations = {}
             self._constraints = set()
             self.input_symbols = list()
-            self._proc = Popen('z3 -t:120 -smt2 -in', shell=True, stdin=PIPE, stdout=PIPE)
+            self._proc = Popen(self._z3_path + ' -t:120 -smt2 -in', shell=True, stdin=PIPE, stdout=PIPE)
 
             # Fix z3 declaration scopes
             self._send("(set-option :global-decls false)")
