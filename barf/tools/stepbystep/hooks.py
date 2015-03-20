da12e5b837e031d63923388bfc4a5dcf8155ab6b
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-03-20 15:52:39 -0300

    Some minor refactoring to step_by_step_taint script.

--- barf/tools/stepbystep/hooks.py
+++ barf/tools/stepbystep/hooks.py
@@ -1,54 +1,62 @@
 from barf.core.dbg.event import CallIntel32
 
-def taint_read(process, event, ir_emulator, initial_taints, open_files, file_mem_mapper, addrs_to_file):
+def open_handler(event, process, open_files):
+    # Get parameters.
+    pathname_ptr, _ = event.get_typed_parameters()[0]
+    file_desc = event.return_value
+
+    filename, truncated = process.readCString(pathname_ptr, 1024)
+    assert(not truncated)
+
+    open_files[file_desc] = {
+        'filename' : filename,
+        'f_pos' : 0
+    }
+
+def read_handler(event, process, ir_emulator, initial_taints, open_files, file_mem_mapper, addrs_to_file):
+    # Get parameters.
+    file_desc, _ = event.get_typed_parameters()[0]
+    buf, _ = event.get_typed_parameters()[1]
+    bytes_read = event.return_value
+
+    # Taint memory address.
+    ir_emulator.set_memory_taint(buf, bytes_read * 8, True)
+
+    file_curr_pos = open_files[file_desc]['f_pos']
+    fmapper = file_mem_mapper.get(file_desc, {})
+
+    # Keep record of inital taints.
+    for i in xrange(0, bytes_read):
+        # Keep track of inital taints.
+        initial_taints.append(buf + i)
+
+        # Keep track of files and current file position.
+        fmapper[buf + i] = file_curr_pos + i
+
+        d_entry = addrs_to_file.get(buf + i, {})
+        l_entry = d_entry.get(file_desc, [])
+        l_entry.append(file_curr_pos + i)
+
+        d_entry[file_desc] = l_entry
+        addrs_to_file[buf + i] = d_entry
+
+        # Set emulator memory
+        data = ord(process.readBytes(buf + i, 1))
+        ir_emulator.write_memory(buf + i, 8, data)
+
+    open_files[file_desc]['f_pos'] = bytes_read
+    file_mem_mapper[file_desc] = fmapper
+
+def process_event(process, event, ir_emulator, initial_taints, open_files, file_mem_mapper, addrs_to_file):
     if isinstance(event, CallIntel32):
         if event.name == "open":
-            pathname_ptr, _ = event.get_typed_parameters()[0]
-            file_desc = event.return_value
-
-            filename, truncated = process.readCString(pathname_ptr, 1024)
-            assert(not truncated)
-
-            open_files[file_desc] = {
-                'filename' : filename,
-                'f_pos' : 0
-            }
+            open_handler(event, process, open_files)
 
         if event.name == "fopen":
             raise NotImplementedError
 
         if event.name == "read":
-            file_desc, _ = event.get_typed_parameters()[0]
-            buf, _ = event.get_typed_parameters()[1]
-            bytes_read = event.return_value
-
-            file_curr_pos = open_files[file_desc]['f_pos']
-            fmapper = file_mem_mapper.get(file_desc, {})
-
-            # Taint memory address.
-            ir_emulator.set_memory_taint(buf, bytes_read * 8, True)
-
-            # Keep record of inital taints.
-            for i in xrange(0, bytes_read):
-                fmapper[buf + i] = file_curr_pos + i
-                initial_taints.append(buf + i)
-
-                d_entry = addrs_to_file.get(buf + i, {})
-                l_entry = d_entry.get(file_desc, [])
-                l_entry.append(file_curr_pos + i)
-
-                d_entry[file_desc] = l_entry
-                addrs_to_file[buf + i] = d_entry
-
-                data = ord(process.readBytes(buf + i, 1))
-                print "read @ %s : %x (%s)" % (hex(buf + i), data, chr(data))
-
-                ir_emulator.write_memory(buf + i, 8, data)
-
-            open_files[file_desc]['f_pos'] = bytes_read
-            file_mem_mapper[file_desc] = fmapper
+            read_handler(event, process, ir_emulator, initial_taints, open_files, file_mem_mapper, addrs_to_file)
 
         if event.name == "fwrite":
             raise NotImplementedError
-
-
