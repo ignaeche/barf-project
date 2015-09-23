aff4a88aedfadebc34f823bb3a95df5956c6e0ea
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-09-23 15:40:03 -0300

    Clean up debugger related classes.

--- barf/barf/core/dbg/specs.py
+++ barf/barf/core/dbg/specs.py
@@ -10,10 +10,10 @@ for raw_spec in f.readlines():
     raw_spec = raw_spec.replace(" (", "(")
     raw_spec = raw_spec.replace("  ", " ")
     raw_spec = raw_spec.replace("  ", " ")
-    if raw_spec <> "" and raw_spec[0] <> ";" and (not "SYS_" in raw_spec):
+    if raw_spec != "" and raw_spec[0] != ";" and (not "SYS_" in raw_spec):
         x = raw_spec.split(" ")
         ret = x[0]
         x = x[1].split("(")
         name = x[0]
-        param_types  = x[1].replace(");", "").split(",")
+        param_types = x[1].replace(");", "").split(",")
         specs[name] = [ret] + param_types
