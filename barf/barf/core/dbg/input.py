6a5b72d5a2f956a76a191265433175b62296ccbb
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-09-23 16:50:02 -0300

    Clean up testcase related classes.

--- barf/barf/core/dbg/input.py
+++ barf/barf/core/dbg/input.py
@@ -1,108 +1,106 @@
 import copy
 
-class Input:
+class Input(object):
+    data = None
+    concrete = False
 
-  data = None
-  concrete = False
+    def __init__(self):
+        pass
 
-  def __init__(self):
-    pass
+    def __len__(self):
+        return len(self.data)
 
-  def __len__(self):
-    return len(self.data)
+    def copy(self):
+        return copy.copy(self)
 
-  def copy(self):
-    #print "data:",self.data
-    return copy.copy(self)
+    def isSymbolic(self):
+        return not self.concrete
 
-  def isSymbolic(self):
-    return not self.concrete
+    def isConcrete(self):
+        return self.concrete
 
-  def isConcrete(self):
-    return self.concrete
+    def SetSymbolic(self):
+        self.concrete = False
 
-  def SetSymbolic(self):
-    self.concrete = False
+    def SetConcrete(self):
+        self.concrete = True
 
-  def SetConcrete(self):
-    self.concrete = True
 
 class Arg(Input):
-  def __init__(self, i, data):
-    self.i = i
 
-    self.data = str(data)
-    if ("\0" in data):
-      self.data = self.data.split("\0")[0]
+    def __init__(self, i, data):
+        self.i = i
+        self.data = str(data)
 
-    self.size = len(self.data)
+        if ("\0" in data):
+            self.data = self.data.split("\0")[0]
 
-  def __str__(self):
-    return "Arg("+str(self.i)+") = "+repr(self.data)
+        self.size = len(self.data)
 
-  def GetData(self):
-    return str(self.data)
+    def __str__(self):
+        return "Arg("+str(self.i)+") = "+repr(self.data)
 
-  def GetSize(self):
-    return len(self.data)
+    def GetData(self):
+        return str(self.data)
 
-  def PrepareData(self):
+    def GetSize(self):
+        return len(self.data)
 
-    return self.GetData()
+    def PrepareData(self):
+        return self.GetData()
 
-  def IsValid(self):
-    return self.size > 0
+    def IsValid(self):
+        return self.size > 0
 
-  def __cmp__(self, arg):
-    return cmp(self.i, arg.i)
+    def __cmp__(self, arg):
+        return cmp(self.i, arg.i)
 
-#  def copy(self):
-#    return Arg(self.i, self.data)
+    def GetName(self):
+        if self.concrete:
+            return "cargv_"+str(self.i)
+        else:
+            return "argv_"+str(self.i)
 
-  def GetName(self):
-    if self.concrete:
-      return "cargv_"+str(self.i)
-    else:
-      return "argv_"+str(self.i)
+    def GetType(self):
+        return "arg"
 
-  def GetType(self):
-    return "arg"
 
 class File(Input):
-  def __init__(self, filename, data):
-    self.filename = str(filename)
-    self.data = bytearray(data)
-    self.size = len(data)
 
-  def __str__(self):
-    return "file("+str(self.filename)+") = "+repr(self.data)
+    def __init__(self, filename, data):
+        self.filename = str(filename)
+        self.data = bytearray(data)
+        self.size = len(data)
 
-  def GetData(self):
-    return str(self.data)
+    def __str__(self):
+        return "file("+str(self.filename)+") = "+repr(self.data)
 
-  def GetSize(self):
-    return len(self.data)
+    def GetData(self):
+        return str(self.data)
 
-  def PrepareData(self):
-    if self.filename == "/dev/stdin":
-      with open("Stdin", 'w') as f:
-        f.write(self.data)
+    def GetSize(self):
+        return len(self.data)
 
-      return "< Stdin"
-    else:
-      with open(self.filename, 'w') as f:
-        f.write(self.data)
+    def PrepareData(self):
+        if self.filename == "/dev/stdin":
+            with open("Stdin", 'w') as f:
+                f.write(self.data)
 
-      return None
+            return "< Stdin"
+        else:
+            with open(self.filename, 'w') as f:
+                f.write(self.data)
 
-  def IsValid(self):
-    return True
+            return None
 
-  def GetFilename(self):
-    return self.filename.replace("/", "__")
+    def IsValid(self):
+        return True
 
-  def GetName(self):
-    return "file_"+self.filename.replace("/", "__")
+    def GetFilename(self):
+        return self.filename.replace("/", "__")
 
-  def GetType(self):
-    return "file"
+    def GetName(self):
+        return "file_"+self.filename.replace("/", "__")
+
+    def GetType(self):
+        return "file"
