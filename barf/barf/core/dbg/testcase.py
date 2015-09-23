862cb34313f18c6b7a26e069afab5e15f3fff71e
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-09-23 16:50:02 -0300

    Clean up testcase related classes.

--- barf/barf/core/dbg/testcase.py
+++ barf/barf/core/dbg/testcase.py
@@ -1,54 +1,63 @@
 import os
 import shutil
 
-from input import Arg, File
+from barf.core.dbg.input import Arg
+from barf.core.dbg.input import File
 
 def prepare_inputs(inputs):
-  r = []
-  for input in inputs:
-    arg = input.PrepareData()
-    if not (arg is None):
-      r.append(arg)
+    r = []
 
-  return r
+    for input in inputs:
+        arg = input.PrepareData()
 
+        if not arg is None:
+            r.append(arg)
+
+    return r
 
 def write_testcase(name, program, args, copy=False):
-  try:
-    os.mkdir(name)
-  except:
-    pass
+    try:
+        os.mkdir(name)
+    except:
+        pass
 
-  os.chdir(name)
-  filename = "path.txt"
-  open(filename,"w").write(program)
-  
-  try:
-    os.mkdir("inputs")
-  except:
-    pass
+    os.chdir(name)
 
-  os.chdir("inputs")
-  for i,arg in enumerate(args):
-    if "file:" in arg:
-      #print arg
-      arg = arg.replace("file:","")
-      assert(arg[0] == '/')
-      filename = os.path.split(arg)[-1]
-      #print filename
-      if copy:
-        shutil.copyfile(os.path.realpath(arg),  "file_"+filename)
-      else:
-        os.symlink(os.path.realpath(arg), "file_"+filename)
-      arg = filename
+    filename = "path.txt"
 
-    filename = "argv_"+str(i+1)+".symb"
-    open(filename,"w").write(arg)
+    open(filename, "w").write(program)
 
-  os.chdir("../..") 
+    try:
+        os.mkdir("inputs")
+    except:
+        pass
+
+    os.chdir("inputs")
+
+    for i, arg in enumerate(args):
+        if "file:" in arg:
+            arg = arg.replace("file:", "")
+
+            assert(arg[0] == '/')
+
+            filename = os.path.split(arg)[-1]
+
+            if copy:
+                shutil.copyfile(os.path.realpath(arg), "file_"+filename)
+            else:
+                os.symlink(os.path.realpath(arg), "file_"+filename)
+
+            arg = filename
+
+        filename = "argv_"+str(i+1)+".symb"
+
+        open(filename, "w").write(arg)
+
+    os.chdir("../..")
 
 def GetTestcase(dirf):
     testcase = dict()
+
     os.chdir(GetDir(dirf))
 
     testcase["filename"] = GetCmd(None)
@@ -58,102 +67,89 @@ def GetTestcase(dirf):
     testcase["envs"] = dict()
     testcase["args"] = GetArgs()
     testcase["files"] = GetFiles()
+
     return testcase
 
 def GetDir(filename):
-  dirf = filename.replace(".tar.bz2","")
+    dirf = filename.replace(".tar.bz2", "")
 
-  #os.system("tar -xf "+ filename)
-  #os.system("rm -f -R "+dirf)
-  return dirf
-
+    return dirf
 
 def GetCmd(s):
+    if os.path.exists("path.txt"):
+        f = open("path.txt")
+        x = f.readline()
 
-  if os.path.exists("path.txt"):
-    f = open("path.txt")
-    x = f.readline()
-    return x.replace("\n","").strip(" ")
-  else:
-    return s
+        return x.replace("\n", "").strip(" ")
+    else:
+        return s
 
 def GetArg(n, conc):
+    if conc:
+        filename = "cargv_"+str(n)+".symb"
+        data = open(filename).read()
+        x = Arg(n, data)
+        x.SetConcrete()
+    else:
+        filename = "argv_"+str(n)+".symb"
+        data = open(filename).read()
+        x = Arg(n, data)
+        x.SetSymbolic()
 
-  # filename = "argv_"+str(n)+".symb"
-  # data = open(filename).read()
-  # x = Arg(n, data)
-
-  if conc:
-    filename = "cargv_"+str(n)+".symb"
-    data = open(filename).read()
-    x = Arg(n, data)
-    x.SetConcrete()
-  else:
-    filename = "argv_"+str(n)+".symb"
-    data = open(filename).read()
-    x = Arg(n, data)
-    x.SetSymbolic()
-
-  return x
-
+    return x
 
 def GetArgs():
-  #i = 1
-  r = []
+    r = []
 
-  for _,_,files in os.walk('.'):
-    for f in files:
-      #print f
-      for i in range(10):
-        #print str(i), f
+    for _, _, files in os.walk('.'):
+        for f in files:
+            for i in range(10):
+                if ("cargv_"+str(i)) in f:
+                    x = GetArg(i, True)
 
-        if ("cargv_"+str(i)) in f:
-          x = GetArg(i, True)
-          if x.IsValid():
-            r.append(x)
+                    if x.IsValid():
+                        r.append(x)
 
-          break
+                    break
+                elif ("argv_"+str(i)) in f:
+                    x = GetArg(i, False)
 
-        elif ("argv_"+str(i)) in f:
-          x = GetArg(i, False)
-          if x.IsValid():
-            r.append(x)
+                    if x.IsValid():
+                        r.append(x)
 
-          break
+                    break
 
-  r.sort()
-  #print r
-  for i in range(len(r)):
-    if r[i].i <> i+1:
-      r = r[0:i]
-      break
+    r.sort()
 
-  #print r
-  return r
+    for i in range(len(r)):
+        if r[i].i != i+1:
+            r = r[0:i]
+            break
+
+    return r
 
 def GetFile(filename, source):
-  #size = int(os.path.getsize(source))
-  data = open(source).read()
-  return File(filename, data)
+    data = open(source).read()
+
+    return File(filename, data)
 
 def GetFiles():
+    r = []
+    stdinf = "file___dev__stdin.symb"
 
-  r = []
-  stdinf = "file___dev__stdin.symb"
+    for dir, _, files in os.walk('.'):
+        if dir == '.':
+            for f in files:
+                if (stdinf == f):
+                    r.append(GetFile("/dev/stdin",stdinf))
+                elif ("file_" in f):
+                    filename = f.split(".symb")[0]
+                    filename = filename.split("file_")[1]
+                    filename = filename.replace(".__", "")
 
-  for dir,_,files in os.walk('.'):
-    if dir == '.':
-      for f in files:
-        if (stdinf == f):
-          r.append(GetFile("/dev/stdin",stdinf))
-        elif ("file_" in f):
-          filename = f.split(".symb")[0]
-          #filename = f.replace(".symb","")
-          filename = filename.split("file_")[1]
-          filename = filename.replace(".__", "")
-          x = GetFile(filename,f)
-          if x.IsValid():
-            r.append(x)
+                    x = GetFile(filename, f)
 
-  return r
+                    if x.IsValid():
+                        r.append(x)
 
+    return r
