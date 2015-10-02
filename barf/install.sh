d63b60e06e76effd5e248bf2ca5c3e3f366873d1
Ignacio <ignaeche@gmail.com>
2015-10-02 14:37:41 -0300

    Added local installation for Capstone

--- barf/install.sh
+++ barf/install.sh
@@ -9,6 +9,26 @@ then
     # ------------------------------------------------------------------------ #
     ./install-solvers.sh local
 
+    # Install Capstone
+    # ------------------------------------------------------------------------ #
+    temp_dir=dependencies
+
+    rm -rf $temp_dir
+    mkdir $temp_dir
+    cd $temp_dir
+
+    CAPSTONE_DOWNLOAD_URL="https://pypi.python.org/packages/source/c/capstone/capstone-3.0.4.tar.gz"
+
+    wget $CAPSTONE_DOWNLOAD_URL
+    tar xfz capstone-3.0.4.tar.gz
+    rm -f capstone-3.0.4.tar.gz
+
+    cd capstone-3.0.4/
+    python setup.py install --user
+
+    cd ../..
+    rm -rf $temp_dir
+
     # Install BARF
     # ------------------------------------------------------------------------ #
     python setup.py install --user
